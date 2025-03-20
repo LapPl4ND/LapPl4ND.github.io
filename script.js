03a0044d58576b94bf33cd6265b9fa196afd40c7
LapPl4ND <47531555+LapPl4ND@users.noreply.github.com>
2025-03-20 22:19:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let video = document.getElementById("bg-video");
+    let muteButton = document.getElementById("mute-button");
+
+    muteButton.addEventListener("click", function () {
+        if (video.muted) {
+            video.muted = false;
+            video.volume = 1.0; // S'assurer que le volume est au max
+            this.textContent = "🔊​";
+        } else {
+            video.muted = true;
+            this.textContent = "🔈​";
+        }
+    });
+});
